70c2f0915bc2e2fa02c6b901e53a256e8216ccb3
Mathieu Joly <superpose@minimac.local>
2025-05-06 22:57:10 +0200

    add infinite looping stars

--- src/components/StarSystem.tsx
+++ src/components/StarSystem.tsx
@@ -1,36 +1,69 @@
-import { useEffect, useMemo, useRef } from "react"
+import { use, useEffect, useMemo, useRef } from "react"
 import * as THREE from "three"
 
 import { generatePos } from "../utils/stars"
+import { useFrame } from "@react-three/fiber"
 
 const STAR_COUNT = 100
 
 function StarSystem() {
-  const ref = useRef<THREE.InstancedMesh>(null)
+  const starsRef = useRef<THREE.InstancedMesh>(null)
 
   const starGeometry = useMemo(() => new THREE.BoxGeometry(0.1, 0.1, 0.1), [])
   const starMaterial = useMemo(() => new THREE.MeshNormalMaterial(), [])
 
+  const temp = useMemo(() => new THREE.Matrix4(), [])
+  const tempPos = useMemo(() => new THREE.Vector3(), [])
+
   useEffect(() => {
-    if (!ref?.current) {
+    if (!starsRef?.current) {
       return
     }
 
-    const temp = new THREE.Object3D()
+    const stars = starsRef.current
 
-    for (let i = 0; i < STAR_COUNT; i++) {
-      temp.position.set(generatePos(), generatePos(), 0)
-      temp.updateMatrix()
-      ref.current.setMatrixAt(i, temp.matrix)
+    const t = new THREE.Object3D()
+    let j = 0
+
+    for (let i = 0; i < STAR_COUNT * 3; i += 3) {
+      t.position.set(generatePos(), generatePos(), generatePos())
+      t.updateMatrix()
+
+      stars.setMatrixAt(j++, t.matrix)
     }
 
-    ref.current.instanceMatrix.needsUpdate = true
+    stars.instanceMatrix.needsUpdate = true
   }, [])
+
+  useFrame((_state, delta) => {
+    if (!starsRef?.current) {
+      return
+    }
+
+    const stars = starsRef.current
+
+    for (let i = 0; i < STAR_COUNT; i++) {
+      stars.getMatrixAt(i, temp)
+      tempPos.setFromMatrixPosition(temp)
+
+      if (tempPos.z > 5) {
+        tempPos.z = -5
+      } else {
+        tempPos.z += delta
+      }
+
+      temp.setPosition(tempPos)
+
+      stars.setMatrixAt(i, temp)
+    }
+
+    stars.instanceMatrix.needsUpdate = true
+  })
 
   return (
     <group>
       <instancedMesh
-        ref={ref}
+        ref={starsRef}
         args={[starGeometry, starMaterial, STAR_COUNT]}
       />
     </group>
